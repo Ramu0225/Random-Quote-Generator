0d65e9b09ca8267e71f4b696637f90eebdbe8b96
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2021-04-21 17:50:10 +0200

    added loader

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-const quoteBox = document.getElementById('quote-box');
+
+let apiQuotes = [];
+const quoteCox = document.getElementById('quote-Box');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('newQuoteBtn');
+const loader = document.getElementById('loader');
 
-let apiQuotes = [];
+
+
+//show loading
+function loading(){
+    loader.hidden = false;
+    quoteCox.hidden = true;
+}
+function complete(){
+    if(!loader.hidden){
+        quoteCox.hidden = false;
+        loader.hidden = true;
+    }
+
+}
 // show New Quote
 function newQuote(){
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     //authorText.textContent = quote.author;
+    
     if(!quote.author){
     authorText.textContent = "Unknown";
 
@@ -18,27 +35,32 @@ function newQuote(){
 
     }
     quoteText.textContent = quote.text;
+    complete();
 }
+
 //get quotes from API
-async function getQuotes(){
+    async function getQuotes(){
+   loading();
     const apiUrl='https://type.fit/api/quotes';
-try{
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
-}catch(error){
-    // catch Error Here
-}
+    
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+        }catch(error){
+            getQuotes();// catch Error Here
+    }
+    
+    }
 
-}
 // tweet quote
-function tweetQuote(){
-    const twitterUrl = `http://twitter.com/intent/tweet?text=${quoteText.textContent} -- ${authorText.textContent}`;
-
-    window.open(twitterUrl, '_blank');
-}
+    function tweetQuote(){
+        const twitterUrl = `http://twitter.com/intent/tweet?text=${quoteText.textContent} -- ${authorText.textContent}`;
+        window.open(twitterUrl, '_blank');
+    }
 // event listeners
-newQuoteBtn.addEventListener('click', newQuote);
-twitterBtn.addEventListener('click', tweetQuote);
+    newQuoteBtn.addEventListener('click', newQuote);
+    twitterBtn.addEventListener('click', tweetQuote);
 //on load
-getQuotes();
+  getQuotes();
+
